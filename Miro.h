4ed77823b4ceb18d81a17c24e986138dbff501b2
Fedor Bezruchko <fbezruchko@gmail.com>
2018-11-13 10:20:02 +1000

    Add files via upload

---
+++ Miro.h
@@ -0,0 +1,63 @@
+#ifndef miro_h
+#define miro_h
+
+#include "Chassis.h"
+#include "Device.h"
+
+char MIRO_VERSION[] = "v1.0.0";
+
+#define MAX_DEVICES 16	//Максимальное число устройств, которые можно подключить к роботу.
+#define ROBOT_DIAMETER (0.16) //ђасстояние между левым и правым колесом (метров)
+
+#define LEFT 0
+#define RIGHT 1
+
+class Miro {
+public:
+
+	Chassis chassis;
+	
+	void Init();
+	
+	void Sync();
+
+	/*движение робота.
+	lin_speed - линейная скорость робота (м/с),
+	ang_speed - угловая скорость робота (град/сек),
+	dist - длина пути, которую нужно преодолеть роботу (м),
+	*/
+	int MoveDist(float lin_speed, float ang_speed, float dist); //Движение робота с заданными линейной и угловыми скоростями
+
+	/*поворот робота на месте.
+	ang - угол поворота,
+	ang_speed - угловая скорость поворота (град/сек),
+	*/
+	int RotateAng(float ang, float ang_speed);
+
+	/*возвращает угловую скороть шасси (рад/сек).*/
+	float getAngSpeedRad();
+
+	/*возвращает угловую скороть шасси (град/сек).*/
+	float getAngSpeed();
+
+	/*возвращает линейную скороть шасси (м/с).*/
+	float getLinSpeed();
+
+	/*возвращает длину пройденного шасси пути.*/
+	float getPath(); //Не реализовано нормально - при расчете учитываются повороты на месте, когда центр масс робота фактически никуда не движется
+
+	/*подключение устройства к роботу*/
+	int attachDevice(Device *dev);
+
+	int dettachDevice(Device *dev);
+	int dettachDevice(byte DeviceIndex);
+
+	char* version = MIRO_VERSION;
+	
+private:
+
+	Device* devices[MAX_DEVICES];
+	unsigned int _device_count;
+};
+
+#endif

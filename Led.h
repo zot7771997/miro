0e3bad8149869cd5c28923f8b6cf961b17ca6f6b
fbezruchko <fbezruchko@gmail.com>
2019-04-06 01:00:02 +1000

    ALL DELETED

--- Led.h
+++
@@ -1,9 +0,0 @@
-#include "Device.h"
-
-class Led : virtual public Device {
-public:
-	void Init(byte pin);
-	void On(byte value);
-	void On();
-	void Off();
-};
--- README.md
+++
@@ -1,2 +0,0 @@
-# miro
-MIRO Robot API
